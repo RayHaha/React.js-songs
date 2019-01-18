f066911d49e0bb354f50a5f8805e35a0a219f52d
RayHaha <g820324@gmail.com>
2019-01-17 23:00:54 -0800

    configure connect with MapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,10 +4,17 @@ import { connect } from "react-redux";
 // we use the name export method to write here
 class SongList extends Component{
     render(){
+        console.log(this.props);
         return <div>SongList</div>;
     }
 }
 
-export default connect()(SongList);
+// convention function name
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+    // by this way, in SongList, this.props === { songs: state.songs }
+}
+
+export default connect(mapStateToProps)(SongList);
 // if the function(A) return another function(B)
 // then A()() will return what function B is going to return
