bbc5d811b95274b09bb5e46972435feaaa6885d6
RayHaha <g820324@gmail.com>
2019-01-18 22:18:10 -0800

    build a list with redux data

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,9 +3,27 @@ import { connect } from "react-redux";
 
 // we use the name export method to write here
 class SongList extends Component{
+    renderList(){
+        // take the list of songs and map over them then return jsx
+        return this.props.songs.map((song) => {
+            // use the class name from semantic UI
+            // a key is not necessarily required but highly recommended
+            // anytime that you are building out a list of elements
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     render(){
-        console.log(this.props);
-        return <div>SongList</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
 
